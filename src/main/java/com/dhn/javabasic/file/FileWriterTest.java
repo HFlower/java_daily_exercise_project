d0217c2fe9f47b60bebe044b2644662eb3d1c508
donghuanan@bonc.com.cn <donghuanan@bonc.com.cn>
2020-04-01 14:47:45 +0800

    输出流

---
+++ src/main/java/com/dhn/javabasic/file/FileWriterTest.java
@@ -0,0 +1,23 @@
+package com.dhn.javabasic.file;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @description: 字符输出流
+ * @author: Dong HuaNan
+ * @date: 2020/4/1 14:41
+ */
+public class FileWriterTest {
+    public static void main(String[] args) {
+        try (FileWriter fileWriter = new FileWriter("poem.txt")){
+            fileWriter.write("锦瑟 - 李商隐\r\n");
+            fileWriter.write("锦瑟无端五十弦，一弦一柱思华年。\r\n");
+            fileWriter.write("庄生晓梦迷蝴蝶，望帝春心托杜鹃。\r\n");
+            fileWriter.write("沧海月明珠有泪，蓝田日暖玉生烟。\r\n");
+            fileWriter.write("此情可待成追忆，只是当时已惘然。\r\n");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
