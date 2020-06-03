f455e555c913cf3a1178533560e34ebf61cc098c
donghuanan@bonc.com.cn <donghuanan@bonc.com.cn>
2020-06-03 15:02:31 +0800

    redis案例：控制单元

--- src/main/java/com/dhn/redis/JedisTest.java
+++ src/main/java/com/dhn/redis/JedisTest.java
@@ -3,6 +3,9 @@ package com.dhn.redis;
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @description: 认识Jedis
  * @author: Dong HuaNan
@@ -17,6 +20,20 @@ public class JedisTest {
 //        jedis.set("name","花花");
         String name = jedis.get("name");
         System.out.println(name);
+//        jedis.lpush("list1","a","b","c");
+//        jedis.rpush("list1","x");
+        List<String> list1 = jedis.lrange("list1",0,-1);
+        for (String s:list1) {
+            System.out.println(s);
+        }
+        System.out.println(jedis.llen("list1"));
+
+        jedis.hset("hash1","a1","a1");
+        jedis.hset("hash1","a2","a2");
+        jedis.hset("hash1","a3","a3");
+        Map<String,String> hash1 = jedis.hgetAll("hash1");
+        System.out.println(hash1);
+        System.out.println(jedis.hlen("hash1"));
         //关闭jedis
         jedis.close();
     }
---
+++ src/main/java/com/dhn/redis/service/Service.java
@@ -0,0 +1,72 @@
+package com.dhn.redis.service;
+
+import lombok.val;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisDataException;
+
+/**
+ * @description: 控制单元
+ * @author: Dong HuaNan
+ * @date: 2020/6/3 14:35
+ */
+public class Service {
+    private String id;
+    private int num;
+    public Service(String id,int num){
+        this.id = id;
+        this.num = num;
+    }
+
+    public void service() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        String value = jedis.get("compid:" + id);
+        try {
+            //判断该值是否存在
+            if (value == null) {
+                //不存在，创建该值
+                jedis.setex("compid:" + id, 5, Long.MAX_VALUE - num + "");
+            } else {
+                //存在，自增，调用业务
+                Long val = jedis.incr("compid:" + id);
+                business(id,num - (Long.MAX_VALUE-val));
+            }
+        } catch (JedisDataException e) {
+            System.out.println("使用已经达到次数上限，请升级会员级别");
+            return;
+        } finally {
+            jedis.close();
+        }
+    }
+
+    public void business(String id,Long val) {
+        System.out.println("用户："+id+"  业务操作执行第"+val+"次");
+    }
+}
+
+class MyThread extends Thread {
+    Service sc;
+    public MyThread(String id,int num) {
+        sc = new Service(id,num);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            sc.service();
+            try {
+                Thread.sleep(300L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MyThread mt1 = new MyThread("初级用户",10);
+        MyThread mt2 = new MyThread("高级用户",30);
+        mt1.start();
+        mt2.start();
+    }
+}
